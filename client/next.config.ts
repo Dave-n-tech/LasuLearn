3ce52d1f4cd752c42a34794dc1932fc4fd7a5d2c
Dave-n-tech <davefast2003@gmail.com>
2025-09-12 13:33:28 +0100

    style: reorder eslint configuration for better structure in next.config.ts

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,14 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     remotePatterns: [
       new URL("https://images.unsplash.com/**"),
       new URL("https://plus.unsplash.com/**"),
     ],
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
   },
 };
 
