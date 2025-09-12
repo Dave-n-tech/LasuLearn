2c9dede418320ee2c5c7ee4ee2c6e31367699cfa
Dave-n-tech <davefast2003@gmail.com>
2025-09-12 23:38:56 +0100

    fix: update axios baseURL to production and modify postinstall script in package.json

--- client/app/api/axios.ts
+++ client/app/api/axios.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:8080/api"
+    baseURL: "https://lasuvlp-2-0.onrender.com/api"
+    // baseURL: "http://localhost:8080/api"
 })
